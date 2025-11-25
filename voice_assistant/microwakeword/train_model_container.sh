9d0499f415f714d92dc83b331c766ed5cf6b378f
Alexander Urdl <alexander.urdl@outlook.com>
2025-11-25 20:26:39 +0100

    added new wake word

--- voice_assistant/microwakeword/train_model_container.sh
+++ voice_assistant/microwakeword/train_model_container.sh
@@ -3,7 +3,7 @@
 
 git clone https://github.com/TaterTotterson/microWakeWord-Trainer-AppleSilicon.git
 cd microWakeWord-Trainer-AppleSilicon
-./train_microwakeword_macos.sh "hey_wuud_hows"
+./train_microwakeword_macos.sh "hey_kom_pyu_tah"
 
 # I had to copy the contents from /opt/homebrew/share/espeak-ng-data/ to .venv/lib/python3.11/site-packages to resolve some errors
 cp -r /opt/homebrew/share/espeak-ng-data/* .venv/lib/python3.11/site-packages/espeakng/data/
